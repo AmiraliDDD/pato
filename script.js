7224199bb21b25ea7c4b5ac14b174b6a6793ed12
Amirali <amirali04.d@gmail.com>
2023-12-18 04:35:57 -0800

    js was added

---
+++ script.js
@@ -0,0 +1,13 @@
+let bars = document.querySelector(".bars");
+let aside = document.getElementById("menu");
+
+function showMenu(){
+aside.classList.toggle("enable")
+}
+
+
+
+
+
+
+bars.addEventListener("click",showMenu);
